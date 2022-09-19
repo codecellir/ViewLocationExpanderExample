e2e2e166165e708cf2e7930ff92cba973d921b32
Sohrab Gheisari <sohrabgheisari@hotmail.com>
2022-09-19 08:23:55 +0430

    init

---
+++ ViewLocationExpanderExample/Infrastructure/MobileViewLocationExpander.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace ViewLocationExpanderExample.Infrastructure
+{
+    public class MobileViewLocationExpander : IViewLocationExpander
+    {
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            var finalViewLocations=new List<string>();
+
+            if (context.Values.ContainsKey("CodeCellMobileKey"))
+            { 
+                foreach (var viewLocation in viewLocations)
+                {
+                    finalViewLocations.Add(viewLocation.Replace(".cshtml", ".mobile.cshtml"));
+                }
+            }
+            finalViewLocations.AddRange(viewLocations);
+
+            return finalViewLocations;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            var userAgent = context.ActionContext.HttpContext.Request.Headers["User-Agent"].ToString();
+            if (userAgent.RequestFromMobileDevice())
+            {
+                context.Values["CodeCellMobileKey"] = "mobile";
+            }
+        }
+    }
+}
---
+++ ViewLocationExpanderExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ViewLocationExpanderExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
